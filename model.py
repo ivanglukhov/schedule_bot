5381ca0f680d7335ea8fce5176454b570f5fb438
ivanglukhov <90202979+ivanglukhov@users.noreply.github.com>
2023-12-09 14:36:48 +0300

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ import sqlite3
 class schedule():
 	connection = 0
 	cursor = ''
-	last_id = 2
 	def __init__(self):
 		self.connection = sqlite3.connect('schedule.db', check_same_thread=False)
 		self.cursor = self.connection.cursor()
@@ -11,33 +10,28 @@ class schedule():
 
 		self.cursor.execute('CREATE TABLE IF NOT EXISTS'+
                        '`day` ('+
-	                   '`id` INT NOT NULL,'+
+	                   '`id` INTEGER PRIMARY KEY AUTOINCREMENT,'+
 	                   '`date` DATE NOT NULL,'+
 	                   '`lessons_count` int NOT NULL,'+
 	                   '`place` varchar NOT NULL,'+
-	                   '`lessons` varchar NOT NULL,'+
-	                   'PRIMARY KEY (`id`)'+
+	                   '`lessons` varchar NOT NULL'+
                        ');')
 
 		self.cursor.execute('CREATE TABLE IF NOT EXISTS' +
 						'`lesson` (' +
-						'`id` INT NOT NULL,' +
+						'`id` INTEGER PRIMARY KEY AUTOINCREMENT,' +
 						'`teacher` varchar NOT NULL,' +
 						'`theme` varchar NOT NULL,' +
 						'`time` varchar NOT NULL,' +
 						'`home_work` varchar NOT NULL,' +
-						'`replace` varchar NOT NULL,' +
-						'PRIMARY KEY (`id`)' +
+						'`replace` varchar NOT NULL' +
 						');')
 
 
 	def create_day(self, date, lessons_count, place, lessons):
-		print('INSERT INTO day(id,date,lessons_count,place,lessons)'+
-							f' VALUES({self.last_id}, "{date}",{lessons_count},"{place}","{lessons}")')
-		self.cursor.execute('INSERT INTO day(id,date,lessons_count,place,lessons)'+
-							f' VALUES({self.last_id}, "{date}",{lessons_count},"{place}","{lessons}")')
+		self.cursor.execute('INSERT INTO day(date,lessons_count,place,lessons)'+
+							f' VALUES("{date}",{lessons_count},"{place}","{lessons}")')
 		self.connection.commit()
-		self.last_id = self.last_id + 1
 	def select_all_days(self):
 		self.cursor.execute('SELECT * from day')
 		return self.cursor.fetchall()
